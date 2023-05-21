bb363f6ce0423b5623006d25818a45947a81812c
Drew Smith <drgh056@gmail.com>
2023-05-21 01:16:32 -0500

    Added product routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
+// Imports required files/methods
 const router = require('express').Router();
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
+// routes to appropriate api
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,16 +3,32 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+// get all products along with cateogries and tags
+router.get('/', async (req, res) => {
+  try {
+    const allProducts = await Product.findAll(
+      {
+        include: [{model: Category}, {model: Tag, through: ProductTag}]
+      }
+    );
+    
+    res.status(200).json(allProducts);
+  } catch (err) {
+      res.status(500).json(err);
+  }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+// get one product based on ID, including Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: Tag, through: ProductTag}]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+      res.status(500).json(err);
+  }
+  
 });
 
 // create new product
@@ -89,8 +105,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+// Deletes product with given id placed in url param, responds with message indicating success or failure
+router.delete('/:id', async (req, res) => {
+  try {
+    const productdata = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productdata) {
+      res.status(404).json({ message: 'No product found for this id.' });
+      return;
+    }
+
+    res.status(200).json({ message: 'This product has been removed.' });
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
